5cd53cbfe387635734688452e1282566b2f4e0e7
Jakob <jakobfred1999@gmail.com>
2024-10-06 08:49:25 +1300

    Added styling

--- CatFactsApi/obj/Debug/net8.0/CatFactsApi.AssemblyInfo.cs
+++ CatFactsApi/obj/Debug/net8.0/CatFactsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatFactsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3046fcc698b115ced7e533bf6483493b0349707")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatFactsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatFactsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CatFactsFrontend/obj/Debug/net8.0/CatFactsFrontend.AssemblyInfo.cs
+++ CatFactsFrontend/obj/Debug/net8.0/CatFactsFrontend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatFactsFrontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3046fcc698b115ced7e533bf6483493b0349707")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatFactsFrontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatFactsFrontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
